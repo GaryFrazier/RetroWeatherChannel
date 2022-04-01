544656635e91767d05bd617eac236fdbac714c7f
frazier_g <garyfrazier95@yahoo.com>
2022-04-01 11:49:39 -0400

    start extended forecast layout

---
+++ retro-weather-channel/src/views/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ExtendedForecast from '../extendedForcast/ExtendedForecast';
+
+function App() {
+  return (
+    <div className="App crt">
+        <ExtendedForecast />
+    </div>
+  );
+}
+
+export default App;
---
+++ retro-weather-channel/src/views/extendedForcast/ExtendedForecast.js
@@ -0,0 +1,16 @@
+import './ExtendedForecast.css';
+
+function ExtendedForecast() {
+  return (
+    <div className="extended-forecast">
+        <div className='main-content'>
+          Information from across the nation
+        </div>
+        <div className='footer'>
+          Clear
+        </div>
+    </div>
+  );
+}
+
+export default ExtendedForecast;
