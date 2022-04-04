3d7c804098b83131ccf9fab8071ca5f7e3fc9eb5
frazier_g <garyfrazier95@yahoo.com>
2022-04-03 22:57:45 -0400

    add data to panel

--- retro-weather-channel/src/views/extendedForcast/ExtendedForecast.js
+++ retro-weather-channel/src/views/extendedForcast/ExtendedForecast.js
@@ -2,35 +2,64 @@ import './ExtendedForecast.css';
 import React, { useEffect } from 'react';
 import Panel from './panel/Panel'
 import Header from './header/Header'
-import axios from 'axios'
+import axios from 'axios';
 import { API_TOKEN } from '../../env';
+import moment from 'moment';
+
 const ExtendedForecast = () => {
   const [data, setData] = React.useState([])
 
   useEffect(() => {
-    if (data?.id) return;
+    if (data?.lat) return;
 
-    axios
-      .get(`https://api.openweathermap.org/data/2.5/forecast?lat=55&lon=55&appid=${API_TOKEN}`)
-      .then((res) => {
-        console.log(res.data)
-        setData(res.data)
-      })
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&exclude=current,minutely,hourly&units=imperial&appid=${API_TOKEN}`)
+          .then((res) => {
+            console.log(res.data)
+            setData(res.data)
+          })
+      }, () => {
+        axios
+        .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${25.7617}&lon=${-80.1918}&exclude=current,minutely,hourly&units=imperial&appid=${API_TOKEN}`)
+        .then((res) => {
+          console.log(res.data)
+          setData(res.data)
+        });
+      });
+    } else {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${25.7617}&lon=${-80.1918}&exclude=current,minutely,hourly&units=imperial&appid=${API_TOKEN}`)
+        .then((res) => {
+          console.log(res.data)
+          setData(res.data)
+        });
+    }
+    
   });
 
-  
+  if (!data?.lat) return;
+
+  const day1 = data.daily[0];
+  day1.day = moment().local().format('ddd');
+  const day2 = data.daily[1];
+  day2.day = moment().local().add(1, 'days').format('ddd');
+  const day3 = data.daily[2];
+  day3.day = moment().local().add(2, 'days').format('ddd');
+
   return (
     <div className="extended-forecast">
         <div className='main-content'>
           <div className='header-section'>
             <div className="header-spacer"/>
-            <Header />
+            <Header city={"city"} country={"country"}/>
             <div className="header-spacer"/>
           </div>
           <div className='panel-section'>
-            <Panel />
-            <Panel />
-            <Panel />
+            <Panel data={day1} />
+            <Panel data={day2} />
+            <Panel data={day3} />
           </div>
         </div>
         <div className='footer'>
--- retro-weather-channel/src/views/extendedForcast/panel/Panel.js
+++ retro-weather-channel/src/views/extendedForcast/panel/Panel.js
@@ -1,27 +1,57 @@
 import './Panel.css';
 
-function Panel() {
+function gifLookup(code) {
+    switch(code) {
+        case "Thunderstorm":
+            return "/CC_TStorm.gif";
+        case "Drizzle":
+            return "/EF_ScatShowers.gif";
+        case "Rain":
+            return "/CC_Rain.gif";
+        case "Snow":
+            return "/CC_Snow.gif";
+        case "Clouds":
+            return "/CC_Cloudy.gif";
+        case "Mist":
+        case "Smoke":
+        case "Haze":
+        case "Dust":
+        case "Fog":
+        case "Sand":
+        case "Dust":
+        case "Ash":
+        case "Squall":
+        case "Tornado":
+            return "/CC_Fog.gif";
+        default:
+            return "/CC_Clear1.gif";
+    }
+}
+function Panel(props) {
+  const data = props.data;
+  if (!data) return;
+
   return (
     <div className="extended-forecast-panel">
         <div className="extended-forecast-panel-border">
             <div className="extended-forecast-panel-content">
                 <div className="day-container">
-                    THU
+                    {data.day}
                 </div>
                 <div className="weather-icon-container">
-                    <img src="/CC_Rain.gif" />
+                    <img src={gifLookup(data?.weather[0].main)} />
                 </div>
                 <div className="weather-status-container">
-                    PARTLY CLOUDY
+                    {data?.weather[0].description}
                 </div>
                 <div className="temperature-container">
                     <div className="low-container">
                         <span className="low-label">Lo</span>
-                        <span className="low-value">35</span>
+                        <span className="low-value">{Math.floor(data?.temp.min)}</span>
                     </div>
                     <div className="high-container">
                         <span className="high-label">Hi</span>
-                        <span className="high-value">70</span>
+                        <span className="high-value">{Math.floor(data?.temp.max)}</span>
                     </div>
                 </div>
             </div>
